0f7ea7995aa1ea2a9d7c2cf3e21027e9dd512a04
ElsevarAsadov <elseverclash57@gmail.com>
2023-10-18 13:02:19 +0400

    done

---
+++ ConsoleApp1/Extensions.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace IDK2
+{
+    internal static class Extensions
+    {
+
+        // ------- TASK1 - START -----//
+
+        public static int MatchWordCount(this string originalStr, string matchStr, bool isGreedy = false)
+        {
+
+            bool found = false;
+
+            int count = 0;
+
+            string currentStack = "";
+
+            int cursor = 0;
+
+            char before = originalStr[0];
+
+
+
+            if (!isGreedy)
+            {
+                for (int i = 0; i < originalStr.Length; i++)
+                {
+                    char c = originalStr[i];
+
+                    if (cursor == matchStr.Length - 1)
+                    {
+                        found = true;
+                        cursor = 0;
+
+
+
+
+
+
+                        if (i != originalStr.Length - 1) currentStack = "";
+                        else currentStack += c;
+
+                        if (i + 1 < originalStr.Length && originalStr[i + 1] == ' ')
+                        {
+                            count++;
+                        }
+
+
+
+                        else
+                        {
+                            currentStack = "";
+                            count++;
+                            cursor = 0;
+                        }
+
+
+                    }
+
+
+                    else if (c == matchStr[cursor] && (c == before || (cursor != 0 && currentStack.ToCharArray().Contains(currentStack[cursor - 1])) || before == ' '))
+                    {
+                        currentStack += matchStr[cursor];
+                        cursor++;
+                    }
+
+
+
+
+                    else
+                    {
+                        currentStack = "";
+                        cursor = 0;
+                    }
+
+                    before = c;
+                }
+
+                if (currentStack == matchStr)
+                {
+                    count++;
+                }
+
+            }
+
+            else
+            {
+                for (int i = 0; i < originalStr.Length; i++)
+                {
+                    char c = originalStr[i];
+
+                    if (cursor == matchStr.Length - 1)
+                    {
+                        found = true;
+                        currentStack = "";
+                        count++;
+                        cursor = 0;
+                    }
+
+                    else if (c == matchStr[cursor])
+                    {
+                        
+                        currentStack += c;
+                        cursor++;
+                    }
+                    else
+                    {
+                        currentStack = "";
+                        cursor = 0;
+                    }
+                }
+            }
+
+            return found ? count : -1;
+        }
+
+
+
+        // ++++++++ TASK1 - END ++++++//
+
+
+
+
+        // ------- TASK2 - START -----//
+
+        public static bool IsPolindrom(this int integer)
+        {
+            char[] carr = integer.ToString().ToCharArray();
+
+            for (int i = carr.Length - 1; i > -1; i--)
+            {
+                if (carr[i] != carr[carr.Length - i - 1])
+                {
+
+                    return false;
+
+                };
+            }
+
+
+            return true;
+
+        }
+
+
+        // ++++++++ TASK2 - END ++++++//
+
+
+
+
+
+        // ------- TASK3 - START -----//
+
+        public static bool HasAnyDigit(this string str)
+        {
+            foreach (char c in str)
+            {
+                if (Char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+
+        }
+
+        // ++++++++ TASK3 - END ++++++//
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IDK2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * 
+             * isGreedy parametri Regex deki 'g' - flagina uygun gelir.
+             * 
+             * default olaraq greedy kimi yoxlamir.
+             * 
+             */
+
+
+            //TASK1
+            Console.WriteLine("Salam BB206, necəsiniz BB206 əhli ".MatchWordCount("BB206") == 2);
+            Console.WriteLine("Salam BB206, necəsiniz BB206əhli ".MatchWordCount("BB206", true) == 2);
+
+            Console.WriteLine("BB206 BB206 fgfgfgfgBB206gfgfgf gfgfgBB206  BB206 ".MatchWordCount("BB206", true) == 5);
+
+            //TASK2
+            Console.WriteLine(3553.IsPolindrom() == true);
+            Console.WriteLine(1553.IsPolindrom() == false);
+
+            //TASK3
+            Console.WriteLine("BB206 is a codder team".HasAnyDigit() == true);
+            Console.WriteLine("Alex is a codder".HasAnyDigit() == false);
+
+        }
+    }
+}
